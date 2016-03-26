a97c3194475ac0df33a64ffc91d87e999f98ff07
ianuzzi <art@ianuzzi.us>
2016-03-26 16:47:48 -0400

    Watchers and the Digest Loop

--- app.js
+++ app.js
@@ -2,14 +2,12 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$filter', function($scope, $filter){
+angularApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout){
 
-	$scope.handle = '';
-
-	$scope.lowercasehandle = function() {
-		return $filter('lowercase')($scope.handle);
-	}
-
+	$scope.name = "Art";
+	$timeout(function(){
+		$scope.name = "Everybody";
+	}, 3000);
 }]);
 
 
