27cde3a331e780220cab1c3711980903bc48cea2
ianuzzi <art@ianuzzi.us>
2016-03-26 15:53:10 -0400

    Directives and Two Way Data Binding

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout){
+angularApp.controller('mainController', ['$scope', '$filter', function($scope, $filter){
 
-	$scope.name = "Art";
-	$timeout(function(){
-		$scope.name = "Everybody";
-	}, 3000);
+	$scope.handle = '';
+
+	$scope.lowercasehandle = function() {
+		return $filter('lowercase')($scope.handle);
+	}
+
 }]);
 
 
