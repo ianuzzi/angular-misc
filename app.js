d21c8258a8ac6959d37c3e7459bcd001114ac8bb
ianuzzi <art@ianuzzi.us>
2016-03-29 17:41:20 -0400

    Routing, Templates and Controllers - Part 1

--- app.js
+++ app.js
@@ -1,60 +1,37 @@
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngRoute']);
+
+// ROUTER
+
+angularApp.config(function($routeProvider){
+
+	$routeProvider
+
+		.when('/', {
+			templateUrl: 'pages/main.html', // get this html
+			controller: 'mainController' // and bind it to this controller
+		})
+
+		.when('/second', {
+			templateUrl: 'pages/second.html', // get this html
+			controller: 'secondController' // and bind it to this controller
+		})
+
+});
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$filter', '$http', 
-									function($scope, $filter, $http){
+angularApp.controller('mainController', ['$scope', '$log', function($scope, $log){
 
-	$scope.handle = '';
-
-	$scope.lowercasehandle = function() {
-		return $filter('lowercase')($scope.handle);
-	};
-
-	$scope.characters = 5;
-
-	// A traditional call to XMLHttpRequest would look like:
-
-	// var rulesrequest = new XMLHttpRequest();
-	// rulesrequest.onreadystatechange = function() {
-
-	// 	$scope.apply(function(){
-	// 		if(rulesrequest.readyState == 4 && rulesrequest.status == 200){
-	// 			$scope.rules = JSON.parse(rulesrequest.responseText);
-	// 		}
-	// 	});
-	// };
-
-	// rulesrequest.open("GET", "./api/rules.json", true);
-	// rulesrequest.send();
-
-	// We can do it a lot easier with Angular using:
-
-	$http.get('./api/rules.json')
-		.success(function(result){
-			$scope.rules = result;
-		})
-		.error(function(data, status){
-			console.log(data);
-		});
-
-	$scope.newRule = '';
-
-	$scope.addRule = function(){
-		$http
-			.post('/api/rules.json', {'newRule': $scope.newRule})
-			.success(function(result){
-				$scope.rules = result; // update the rules with the returned value inclusding the new rule
-				$scope.newRule = ''; // clear the newRule variable (clears the textbox as well)
-			})
-			.error(function(data, status){
-				console.log(data);
-				$scope.newRule = ''; // clear the newRule variable (clears the textbox as well)
-
-			});
-	};
+	$scope.name = 'Main';
 
 }]);
+
+angularApp.controller('secondController', ['$scope', '$log', function($scope, $log){
+
+	$scope.name = 'Second'
+
+}]);
+
 
 
 
